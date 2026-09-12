469e032d03bd6b756d275bd99b1f0675b0db4dd8
Kyaku23 <kyaku23desu@gmail.com>
2026-09-12 10:30:39 +0700

    update code practice 2

--- main2.js
+++ main2.js
@@ -3,3 +3,11 @@ console.log("  ***  ");
 console.log(" ***** ");
 console.log("*******");
 console.log("  |||  ");
+
+console.log("FISH FISH FISH");
+console.log("<><      <><  ");
+console.log("    <><       ");
+comsole.log("<><      <><  ");
+
+console.log("MEAW MEAW");
+console.log("/\_/\");
