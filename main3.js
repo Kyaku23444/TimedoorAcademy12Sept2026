787cc6c8c6a719b415f07d9a0c7c6fadf3f175fb
Kyaku23 <kyaku23desu@gmail.com>
2026-09-19 09:13:29 +0700

    update code practice 3

---
+++ main3.js
@@ -0,0 +1,8 @@
+console.log("Learning Javascript");
+document.write("Timedoor Academy");
+console.log(" /\_/\ ");
+console.log("( 0.0 )");
+console.log(" > ^ < ");
+
+var name = "Alice";
+console.log("Hello, " + name + "!");
