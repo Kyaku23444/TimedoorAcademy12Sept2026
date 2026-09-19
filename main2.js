2c6ced6fad273e9194ec4c27bd2520161d7ac069
Kyaku23 <kyaku23desu@gmail.com>
2026-09-19 09:10:07 +0700

    update main 2 #2

--- main2.js
+++ main2.js
@@ -3,3 +3,13 @@ console.log("  ***  ");
 console.log(" ***** ");
 console.log("*******");
 console.log("  |||  ");
+
+console.log("FISH FISH FISH");
+console.log("<><    <><    ");
+console.log("  <><         ");
+console.log("<><    <><    ");
+
+console.log("MEAW MEAW");
+console.log("  /\_/\  ");
+console.log("( 0 . 0 )");
+console.log(">   ^   <");
