5debecdb2d4220a6cfd14dbadcc6f95cdc63289e
Kyaku23 <kyaku23desu@gmail.com>
2026-09-19 10:20:53 +0700

    operator practice update 1

---
+++ operator1.js
@@ -0,0 +1,12 @@
+var topUpSteamWallet = 50;
+var discount = 2 * 3;
+var totalPayment = 50 * 3;
+var totalDiscountedPayment = totalPayment -  discount;
+console.log(totalDiscountedPayment);
+
+var gameScore = 1858;
+var xpAdd = 50;
+var scoreDivide = 1858 / 500;
+var xpMultiplier = xpAdd * scoreDivide;
+
+console.log(xpMultiplier);
---
+++ operator2.js
@@ -0,0 +1,16 @@
+var a = 15;
+a += 2;
+var b = 23;
+b -= 3;
+var c = 228;
+c /= 76;
+var d = 500;
+d *= 2;
+var e = 100;
+e /= 25;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
